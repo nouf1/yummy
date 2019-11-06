96bb2f700d08e584a2ce39db3911fd055dbd5a39
نوف الوزير <noufalmatroudi@nwfs-MacBook-Air.local>
2019-11-06 16:28:42 +0300

    Updates

---
+++ precache-manifest.a62ba0a05137261b56714cabe52c1b2a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a46082057423d51f3cf80fd1535d96e",
+    "url": "/yummy/index.html"
+  },
+  {
+    "revision": "30a8c40f8b6ac853b9c5",
+    "url": "/yummy/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "f611228a111dc9331b20",
+    "url": "/yummy/static/css/main.94fad014.chunk.css"
+  },
+  {
+    "revision": "30a8c40f8b6ac853b9c5",
+    "url": "/yummy/static/js/2.267a0f6e.chunk.js"
+  },
+  {
+    "revision": "f611228a111dc9331b20",
+    "url": "/yummy/static/js/main.23891c91.chunk.js"
+  },
+  {
+    "revision": "3bffaf9f750059df56db",
+    "url": "/yummy/static/js/runtime-main.f491469e.js"
+  },
+  {
+    "revision": "014a0cb3f95dad8839192ac185c15a9e",
+    "url": "/yummy/static/media/burger4.014a0cb3.jpg"
+  },
+  {
+    "revision": "b8b152541e71e61b4e3989cfce94622c",
+    "url": "/yummy/static/media/image2.b8b15254.jpeg"
+  },
+  {
+    "revision": "c50923091d86e1bad67f39a19e930c7a",
+    "url": "/yummy/static/media/image3.c5092309.jpg"
+  },
+  {
+    "revision": "dcaa550ec8c9fc3bc3b3d31b6244393d",
+    "url": "/yummy/static/media/image4.dcaa550e.jpg"
+  },
+  {
+    "revision": "8b203bec9f296f4caf51b4dd2125c475",
+    "url": "/yummy/static/media/image5.8b203bec.jpg"
+  },
+  {
+    "revision": "2a75625e11c2debd693856a8b0dfc022",
+    "url": "/yummy/static/media/slide1.2a75625e.jpeg"
+  },
+  {
+    "revision": "ec8948120bbeb5b982d8bc5ab63d8750",
+    "url": "/yummy/static/media/slide3.ec894812.jpeg"
+  },
+  {
+    "revision": "b2c1268ca11dd04f932c0d254e15b9fd",
+    "url": "/yummy/static/media/sliderchicken.b2c1268c.jpg"
+  }
+]);
